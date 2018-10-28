23d52d0dd0145c99c0aba8c12b9561432e9f34c1
ArtemZgh <lowershotower@gmail.com>
2018-10-28 20:31:58 +0300

    feastyle: comments in recursion.js

--- recursion.js
+++ recursion.js
@@ -1,3 +1,4 @@
+// Не сделана
 let tree = {
     "value": 100,
     "left": {
@@ -18,41 +19,27 @@ function recursion(tr, lvl = 0) {
     var res2;
     var val;
     for (let key in tr) {
-        
-            if (key == 'value') {
-                val = tr[key];
-                console.log(val + ' ' + lvl)
+        if (key == 'value') {
+            val = tr[key];
+        }
+        if (key == 'left') {
+            res1 = recursion(tr[key], (lvl + 1));
+            if (tempArr[lvl + 1] == undefined) {
+                tempArr[lvl + 1] = [].concat(res1[lvl + 1]);
+            } else {
+                tempArr[lvl + 1] = [...tempArr[lvl + 1], ...res1[lvl + 1]]
             }
-            if (key == 'left') {
-                res1 = recursion(tr[key], (lvl + 1));
-                if (tempArr[lvl + 1] == undefined) {
-                    tempArr[lvl + 1] = [].concat(res1[lvl+1]);
-                } else {
-                    console.log(res1)
-                    console.log(res1 + ' ' + lvl)
-                    
-                    tempArr[lvl + 1] = [...tempArr[lvl + 1], ...res1[lvl + 1]]
-                }
-               
+        }
+        if (key == 'right') {
+            res2 = recursion(tr[key], (lvl + 1));
+            if (tempArr[lvl + 1] == undefined) {
+                tempArr[lvl + 1] = res2[lvl + 1]
+            } else {
+                tempArr[lvl + 1] = [...tempArr[lvl + 1], ...res2[lvl + 1]]
             }
-            if (key == 'right') {
-                res2 = recursion(tr[key], (lvl + 1));
-                if (tempArr[lvl + 1] == undefined) {
-                    tempArr[lvl + 1] = res2[lvl + 1]
-                    console.log(tempArr)
-                    console.log(res2[lvl + 1])
-                } else {
-                    tempArr[lvl + 1] = [...tempArr[lvl + 1], ...res2[lvl + 1]]
-                }
-            }
-            console.log(tempArr)
-            console.log(res2)
-        
+        }
     }
-    console.log(val)
-    tempArr[lvl] =[ val];
-    console.log(tempArr)
-    console.log(tempArr.length)
+    tempArr[lvl] = [val];
     return tempArr;
 }
 
